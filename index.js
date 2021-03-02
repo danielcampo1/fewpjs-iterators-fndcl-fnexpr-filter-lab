e3aa26a4f2851a9f3ca129763d7efe68e155756a
github username <github email address>
2021-03-02 16:40:07 -0600

    commit tree dce68ecaf36b9b0850f1de7e747cbae16648611a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+// // Code your solution here
+// function findMatching(drivers,strings)
+//     return marching list of drivers, and is case insensitve
+
+
+//     function fuzzyMatch(drivers,string)
+//     return all drivers whose name begins with a a letter
+
+//     function matchName(drivers,string)
+//     drivers is JS obj(name)
+//     return each name that matches string
+
+function findMatching(drivers, string){
+    return drivers.filter(driver =>
+        driver.toLowerCase() === string.toLowerCase()
+    )}
+
+    function fuzzyMatch(drivers, string) {
+        return drivers.filter( driver => 
+            driver.toLowerCase().indexOf(string.toLowerCase()) === 0 )
+        }
+
+        function matchName(drivers, string) {
+            return drivers.filter( driver => 
+                driver.name === string
+            )
+        }
